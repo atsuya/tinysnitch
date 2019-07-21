825bd4da6315536c10b1a5756e77aa3ce7d0b2ab
Atsuya Takagi <asoftonight@gmail.com>
2019-07-21 13:18:52 +0900

    reverse dns lookup only when it's not local ip

--- tinysnitch.js
+++ tinysnitch.js
@@ -4,6 +4,7 @@ const readline = require('readline')
 const { Resolver } = require('dns').promises
 
 const UfwLogLineParser = require('./ufw-log-line-parser')
+const NetworkUtilities = require('./network-utilities')
 
 const resolver = new Resolver()
 const ufwLogLineParser = new UfwLogLineParser()
@@ -11,25 +12,36 @@ const ufwLogLineParser = new UfwLogLineParser()
 const rl = readline.createInterface({
   input: process.stdin,
 })
-rl.on('line', async (line) => {
-  //console.log(`received: ${line}`)
-
-  try {
-    const message = await ufwLogLineParser.parse(line)
-    //console.log(message)
-    if (message.get('PROTO') !== 'TCP') {
-      return
-    }
-
-    const destinationIp = message.get('DST')
-    const domains = await resolver.reverse(destinationIp)
-    console.log(`${domains.join(',')} [${destinationIp}]`)
-  } catch (exception) {
-    console.log(`Failed to parse: ${exception.message}`)
-  }
-})
 
 process.on('SIGINT', () => {
   console.log('SIGINT detected, exiting.')
   process.exit()
 })
+
+/**
+ * Main.
+ */
+async function main() {
+  const localIps = await NetworkUtilities.localIps()
+  console.log(localIps)
+
+  rl.on('line', async (line) => {
+    //console.log(`received: ${line}`)
+
+    try {
+      const message = await ufwLogLineParser.parse(line)
+
+      const destinationIp = message.get('DST')
+      if (localIps.includes(destinationIp)) {
+        console.log(`skipping: destination=${destinationIp}, ${localIps.join(',')}`)
+        return
+      }
+
+      const domains = await resolver.reverse(destinationIp)
+      console.log(`${domains.join(',')} [${destinationIp}]`)
+    } catch (exception) {
+      console.log(`Failed to parse: ${exception.message}`)
+    }
+  })
+}
+main()
